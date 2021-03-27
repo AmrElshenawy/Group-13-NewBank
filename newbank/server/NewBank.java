708532f212705227f33e0ac9e4af200e01d93fa1
Amhar <af965@bath.ac.uk>
2021-03-27 22:49:17 +0100

    -added fields transactions and handleTransactions -added hashmap for transactions to constructor -added readTransactionFromDB method to read the database and fill the hashmap -added a getter to return the transactions field -added transaction operation to MOVE -added AUDITREPORT protocol which can only be performed by staff -added transaction operation to transferFunds method

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -2,20 +2,30 @@ package newbank.server;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
 
 public class NewBank {
 	
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
 	private DatabaseHandler fullReport = new DatabaseHandler();
+	private HashMap<Account,Transaction> transactions;
+	private TransactionHandler handleTransactions = new TransactionHandler();
 	
 	public NewBank() {
 		customers = new HashMap<>();
 		fillHashMap_fromDB();
+		transactions = new HashMap<>();
+		readTransactionsFromDB();
 	}
 
 	private void fillHashMap_fromDB() {
@@ -26,20 +36,43 @@ public class NewBank {
 				for(int i = 0; i < line.size(); i++){
 					Customer x = new Customer(line.get(1));
 					if(line.size() > 3){
-						x.setAccount(new Account(line.get(4), Double.parseDouble(line.get(5)), Account.AccountType.valueOf(line.get(3).toUpperCase())));
+						x.setAccount(new Account(Integer.parseInt(line.get(4)), Double.parseDouble(line.get(5)), Account.AccountType.valueOf(line.get(3).toUpperCase())));
 					}
 					x.setPassword(line.get(2));
 					x.setCustomerID((x.getFullName() + x.getPassword()).hashCode());
 					customers.put(line.get(1), x);
 					if(line.size() > 6){
-						x.setAccount(new Account(line.get(7), Double.parseDouble(line.get(8)), Account.AccountType.valueOf(line.get(6).toUpperCase())));
+						x.setAccount(new Account(Integer.parseInt(line.get(7)), Double.parseDouble(line.get(8)), Account.AccountType.valueOf(line.get(6).toUpperCase())));
 					}
 					if(line.size() > 9){
-						x.setAccount(new Account(line.get(10), Double.parseDouble(line.get(11)), Account.AccountType.valueOf(line.get(9).toUpperCase())));
+						x.setAccount(new Account(Integer.parseInt(line.get(10)), Double.parseDouble(line.get(11)), Account.AccountType.valueOf(line.get(9).toUpperCase())));
 					}
 				}
 			}
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readTransactionsFromDB() {
+		List<ArrayList<String>> scanOutput;
+		try {
+			scanOutput = handleTransactions.scanFullDB();
+			for(ArrayList<String> line: scanOutput){
+				for(int i = 0; i < line.size(); i++){
+					Account account = new Account(parseInt(line.get(2)));
+					Date dateTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(line.get(1));
+					int senderId = parseInt(line.get(2));
+					String senderName = line.get(3);
+					int receiverId = parseInt(line.get(4));
+					String receiverName = line.get(5);
+					Double amount = parseDouble(line.get(6));
+					String message = line.get(7);
+					Transaction transaction = new Transaction(dateTime, senderId, senderName, receiverId, receiverName, amount, message);
+					transactions.put(account, transaction);
+				}
+			}
+		} catch (FileNotFoundException | ParseException e) {
 			e.printStackTrace();
 		}
 	}
@@ -50,6 +83,10 @@ public class NewBank {
 
 	public HashMap<String,Customer> getCustomers(){
 		return customers;
+	}
+
+	public HashMap<Account,Transaction> getTransactions(){
+		return transactions;
 	}
 	
 	public synchronized CustomerID checkLogInDetails(String userName, String password) throws FileNotFoundException {
@@ -65,7 +102,7 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) throws FileNotFoundException {
-		if(customers.containsKey(customer.getKey())) { //this isn't a safe check - how to change?
+		if(customers.containsKey(customer.getKey())) {
 			String[] requestSplit = request.split(" ");
 			switch(requestSplit[0]) {
 			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
@@ -102,11 +139,11 @@ public class NewBank {
 					else{
 						type = Account.AccountType.CHECKING; // default case
 					} 
-					customers.get(customer.getKey()).setAccount(new Account(accountName,openingBalance,type));
+					customers.get(customer.getKey()).setAccount(new Account(Integer.parseInt(accountName),openingBalance,type));
 				} 
 				else{
 					type = Account.AccountType.CHECKING; //default account type if no type is specified
-					customers.get(customer.getKey()).setAccount(new Account(accountName, openingBalance,type));
+					customers.get(customer.getKey()).setAccount(new Account(Integer.parseInt(accountName), openingBalance,type));
 				} 
 				return "SUCCESS";
 			case "MOVE":
@@ -127,58 +164,80 @@ public class NewBank {
 					} else {
 						accountFrom.modifyBalance(amount, Account.InstructionType.WITHDRAW);
 						accountTo.modifyBalance(amount, Account.InstructionType.DEPOSIT);
+						Date dateTime = new Date();
+						String formattedDate = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(dateTime);
+						int senderId = accountFrom.getAccountId();
+						String senderName = customer.getKey();
+						int receiverId = accountTo.getAccountId();
+						String receiverName = customer.getKey();
+						String message = "payment from "+senderName+" to "+receiverName+" of "+amount+" on "+ formattedDate;
+						Transaction transaction = new Transaction(dateTime, senderId, senderName, receiverId, receiverName, amount, message);
+						transactions.put(accountFrom, transaction);
 						return "SUCCESS! Move transfer complete.";
 					}
 				}
-				case "PAY":
-					//PAY <Amount> <From Users Account> <To Payee's UserName>
-					if (requestSplit.length != 4){
-						return "FAIL";
-					} else {
-						return payPerson(customer, requestSplit );
+			case "PAY":
+				//PAY <Amount> <From Users Account> <To Payee's UserName>
+				if (requestSplit.length != 4){
+					return "FAIL";
+				} else {
+					return payPerson(customer, requestSplit );
+				}
+			case "CONFIRM":
+				DatabaseHandler save = new DatabaseHandler();
+				try {
+					save.saveSession(customers);
+					handleTransactions.saveSession(transactions);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return "ACTION CONFIRMED!";
+			case "DELETE":
+				// DELETE <Customer ID>
+				// DELETE <Customer ID> <AccountType>
+				if(customer.getKey().equalsIgnoreCase("staff")){
+					if(requestSplit.length == 2){
+						for(Map.Entry<String, Customer> record : customers.entrySet()){
+							String user = record.getKey();
+							Customer object = record.getValue();
+							if(Integer.toString(object.getCustomerId()).equals(requestSplit[1])){
+								customers.remove(user);
+								return "Customer ID# " + object.getCustomerId() + " DELETED!";
+							}
+						}
 					}
-				case "CONFIRM":
-					DatabaseHandler save = new DatabaseHandler();
-					try {
-						save.saveSession(customers);
+					else if(requestSplit.length == 3){
+						for(Map.Entry<String, Customer> record :customers.entrySet()){
+							Customer object = record.getValue();
+							if(Integer.toString(object.getCustomerId()).equals(requestSplit[1])){
+								ArrayList<Account> accList = object.getAccounts();
+								for(Account acc : accList){
+									if(acc.getAccountType().toString().equalsIgnoreCase(requestSplit[2])){
+										object.deleteAccount(acc);
+										return "Account type " + requestSplit[2].toUpperCase() + " for customer ID# " + object.getCustomerId() + " DELETED!";
+									}
+								}
+
+							}
+						}
+					}
+				}
+				else{
+					return "NOT A STAFF MEMBER. ACTION DENIED!";
+				}
+			case "AUDITREPORT":
+				// AUDITREPORT
+				if(customer.getKey().equalsIgnoreCase("staff")){
+					try{
+						Report report = new Report();
+						report.generateReport();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					return "ACTION CONFIRMED!";
-				case "DELETE":
-					// DELETE <Customer ID>
-					// DELETE <Customer ID> <AccountType>
-					if(customer.getKey().equalsIgnoreCase("staff")){
-						if(requestSplit.length == 2){
-							for(Map.Entry<String, Customer> record : customers.entrySet()){
-								String user = record.getKey();
-								Customer object = record.getValue();
-								if(Integer.toString(object.getCustomerId()).equals(requestSplit[1])){
-									customers.remove(user);
-									return "Customer ID# " + object.getCustomerId() + " DELETED!";
-								}
-							}
-						}
-						else if(requestSplit.length == 3){
-							for(Map.Entry<String, Customer> record :customers.entrySet()){
-								Customer object = record.getValue();
-								if(Integer.toString(object.getCustomerId()).equals(requestSplit[1])){
-									ArrayList<Account> accList = object.getAccounts();
-									for(Account acc : accList){
-										if(acc.getAccountType().toString().equalsIgnoreCase(requestSplit[2])){
-											object.deleteAccount(acc);
-											return "Account type " + requestSplit[2].toUpperCase() + " for customer ID# " + object.getCustomerId() + " DELETED!";
-										}
-									}
-
-								}
-							}
-						}
-					}
-					else{
-						return "NOT A STAFF MEMBER. ACTION DENIED!";
-					}
-
+					return "SUCCESS";
+				}else{
+					return "NOT A STAFF MEMBER. ACTION DENIED!";
+				}
 			default : return "FAIL";
 			}
 		}
@@ -210,6 +269,7 @@ public class NewBank {
 			return false;
 		}
 	}
+
 	/* Method: payPerson
 	Expected input: String from command line: PAY <Amount> <From Users Account> <To Payee's UserName>
 	assumptions:
@@ -262,6 +322,21 @@ public class NewBank {
 		} else {
 			user.modifyBalance(payment, Account.InstructionType.WITHDRAW);
 			payee.modifyBalance(payment, Account.InstructionType.DEPOSIT);
+			Date dateTime = new Date();
+			String formattedDate = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(dateTime);
+			int senderId = user.getAccountId();
+			String senderName = "";
+			for(Map.Entry<String, Customer> record : customers.entrySet()) {
+				senderName = record.getKey();
+			}
+			int receiverId = payee.getAccountId();
+			String receiverName = "";
+			for(Map.Entry<String, Customer> record : customers.entrySet()) {
+				receiverName = record.getKey();
+			}
+			String message = "payment from "+senderName+" to "+receiverName+" of "+payment+" on "+formattedDate;
+			Transaction transaction = new Transaction(dateTime, senderId, senderName, receiverId, receiverName, payment, message);
+			transactions.put(user, transaction);
 			return "SUCCESS";
 		}
 	}
