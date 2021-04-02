e74e986a578a6beaeb459de2648699acb4eabb2f
Amhar <af965@bath.ac.uk>
2021-04-02 17:00:25 +0200

    -changed joinDate field from Date to LocalDate object since Date is deprecated as of Java 8 -added field numberLoans to track the number of loans a customer has taken out -added new fields to constructor -added incrementNumberLoans method to update the numberLoans field on a transaction event -adeded getNumberLoans method to retrieve the number of loans currently taken out -added getJoinDate method to retrieve the data the customer was crated in the bank -added canTakeLoan method to validate if the customer is allowed to receive a loan -added canOfferLoan method to validate if the customer is allowed to offer a loan

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -2,7 +2,8 @@ package newbank.server;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Customer {
 	
@@ -13,11 +14,16 @@ public class Customer {
 	private String taxId;
 	private int customerId;
 	private String password;
+	private LocalDate joinDate;
+	private int numberLoans;
+
 	
 	public Customer(String name) {
 		accounts = new ArrayList<>();
 		address = new HashMap<>();
 		fullName = name;
+		joinDate = LocalDate.now();
+		numberLoans = 0;
 	}
 	
 	public String accountsToString() {
@@ -72,6 +78,11 @@ public class Customer {
 		dob = birthdate;
 	}
 
+	public int incrementNumberLoans() {
+		numberLoans++;
+		return numberLoans;
+	}
+
 	// getters
 	public ArrayList<Account> getAccounts() {
 		return accounts;
@@ -104,5 +115,48 @@ public class Customer {
 	public String getPassword(){
 		return this.password;
 	}
+
+	public LocalDate getJoinDate(){
+		return joinDate;
+	}
+
+	public int getNumberLoans() {
+		return numberLoans;
+	}
+
+	/* methods check that client does not have more than 3 loans already
+	and has been with the bank for at least 90 days */
+	public boolean canTakeLoan(){
+		Double sumLoans = 0.0;
+		LocalDate acceptDate = joinDate.plus(Period.ofDays(90));
+		for(Account account: this.getAccounts()){
+			for(Transaction transaction: account.getTransactions()){
+				if(transaction.getTransactionType().equals(Transaction.TransactionType.MICROLOAN)){
+					sumLoans =+ transaction.getAmount();
+				}
+			}
+		}
+
+		return this.getNumberLoans() <= 3 && LocalDate.now().isAfter(acceptDate) && sumLoans < 1000 ? true : false;
+	}
+
+	//checks if a customer meets the criteria for offering a loan
+	public boolean canOfferLoan(){
+		Double sumDeposits = 0.0;
+		int count = 0;
+		Double totalBalance = 0.0;
+		for(Account account: this.getAccounts()){
+			totalBalance =+ account.getOpeningBalance();
+			for(Transaction transaction: account.getTransactions()){
+				if(transaction.getTransactionType().equals(Transaction.TransactionType.DEPOSIT)){
+					count++;
+					sumDeposits =+ transaction.getAmount();
+				}
+			}
+		}
+		return sumDeposits >= 1000.0 && count >= 3 && totalBalance > 0 ?  true : false;
+	}
+
+
 
 }
