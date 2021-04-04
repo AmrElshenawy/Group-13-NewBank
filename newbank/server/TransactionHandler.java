f81787ee6119feff16af91c90366dd488395a70d
Amhar <af965@bath.ac.uk>
2021-04-04 14:13:56 +0200

    refactored saveSession() to write a single transaction popped from the ArrayList into the transactions table

--- newbank/server/TransactionHandler.java
+++ newbank/server/TransactionHandler.java
@@ -19,7 +19,7 @@ public class TransactionHandler {
     // This method is primarily used to fill in and refresh the Hashmap with information from the database.
     public List<ArrayList<String>> scanFullDB() throws FileNotFoundException {
         String info = "";   // A full line in the database
-        plainDBContent.clear();
+        //plainDBContent.clear();
         try{
             BufferedReader reader = new BufferedReader(new FileReader(dB));
             info = reader.readLine();
@@ -111,8 +111,9 @@ public class TransactionHandler {
     public void saveSession(ArrayList<Transaction> transactionsList) throws IOException{
         BufferedWriter writer = new BufferedWriter(new FileWriter(dB, true));
         String output = "";
+        Transaction transaction = transactionsList.get(transactionsList.size()-1);
         //for(Transaction transaction: transactions.values()){
-        for(Transaction transaction: transactionsList){
+        //for(Transaction transaction: transactionsList){
             output += "transactionId" + ":" + transaction.getTransactionId() + ",";
             output += "transactionDateTime" + ":" + DateTimeFormatter.ofPattern("MM/dd/yyyy HH.mm.ss").format(transaction.getTransactionDateTime()) + ",";
             output += "senderAccountId" + ":" + String.valueOf( transaction.getSenderId()) + ",";
@@ -123,7 +124,7 @@ public class TransactionHandler {
             output += "message" + ":" + String.valueOf(transaction.getMessage()) + ",";
             output += "type" + ":" + String.valueOf(transaction.getTransactionType()) + ",";
             output += System.lineSeparator();
-        }
+        //}
         writer.write(output);
         writer.close();
     }
