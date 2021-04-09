335f06c626f687d31fc566e85c952a0a7f4bb37b
Amr Elshenawy <amrsamir500@hotmail.com>
2021-04-09 10:55:28 +0100

    Modificaitons to previous Unit Tests

--- Testing/NEWACCOUNT_UnitTest.java
+++ Testing/NEWACCOUNT_UnitTest.java
@@ -16,53 +16,53 @@ public class NEWACCOUNT_UnitTest {
 
     @Test
     @DisplayName("Fails if an account type already exists -> For bhagy: (NEWACCOUNT CHECKING)")
-    public void TEST1() throws FileNotFoundException{
+    public void TESTa() throws FileNotFoundException{
         assertEquals("ERROR. ACCOUNT TYPE ALREADY EXISTS!", bank.processRequest(new CustomerID("bhagy"), "NEWACCOUNT CHECKING"));
         //assertEquals("FAIL", bank.processRequest(new CustomerID("amrelshenawy"), "NEWACCOUNT 500 moneymarket"));
     }
 
     @Test
     @DisplayName("Passes if creating a new account that doesn't exist -> For amrelshenawy: (NEWACCOUNT savings), (NEWACCOUNT moneymarket)")
-    public void TEST2() throws FileNotFoundException{
+    public void TESTb() throws FileNotFoundException{
         assertEquals("SUCCESS", bank.processRequest(new CustomerID("amrelshenawy"), "NEWACCOUNT savings"));
         assertEquals("SUCCESS", bank.processRequest(new CustomerID("amrelshenawy"), "NEWACCOUNT moneymarket"));
     }
 
     @Test
     @DisplayName("Fails if number of accounts exceed 3 -> For bhagy: (NEWACCOUNT MONEYMARKET), (NEWACCOUNT savings)")
-    public void TEST3() throws FileNotFoundException{
+    public void TESTc() throws FileNotFoundException{
         assertEquals("SUCCESS", bank.processRequest(new CustomerID("bhagy"), "NEWACCOUNT MONEYMARKET"));
         assertEquals("MAXIMUM NUMBER OF ACCOUNTS REACHED!", bank.processRequest(new CustomerID("bhagy"), "NEWACCOUNT savings"));
     }
 
     @Test
     @DisplayName("Fails if input arguments are incorrect -> For bhagy: (NEWACCOUNT 500 moneymarket)")
-    public void TEST4() throws FileNotFoundException{
+    public void TESTd() throws FileNotFoundException{
         assertEquals("INCORRECT COMMAND ENTERED!", bank.processRequest(new CustomerID("bhagy"), "NEWACCOUNT 500 moneymarket"));
     }
 
     @Test
     @DisplayName("Passes with account type case-insensitive -> For amrelshenawy: (NEWACCOUNT SAVINGS), (NEWACCOUNT moneymarket)")
-    public void TEST5() throws FileNotFoundException{
+    public void TESTe() throws FileNotFoundException{
         assertEquals("SUCCESS", bank.processRequest(new CustomerID("amrelshenawy"), "NEWACCOUNT SAVINGS"));
         assertEquals("SUCCESS", bank.processRequest(new CustomerID("amrelshenawy"), "NEWACCOUNT moneymarket"));
     }
 
     @Test
     @DisplayName("Fails if no account type is included -> For bhagy: (NEWACCOUNT 500)")
-    public void TEST6() throws FileNotFoundException{
+    public void TESTf() throws FileNotFoundException{
         assertEquals("PLEASE PROVIDE CHECKING, SAVINGS OR MONEYMARKET AS TYPE.", bank.processRequest(new CustomerID("bhagy"), "NEWACCOUNT 500"));
     }
 
     @Test
     @DisplayName("Fails if incorrect account type entered -> For john: (NEWACCOUNT overdraft)")
-    public void TEST7() throws FileNotFoundException{
+    public void TESTg() throws FileNotFoundException{
         assertEquals("PLEASE PROVIDE CHECKING, SAVINGS OR MONEYMARKET AS TYPE.", bank.processRequest(new CustomerID("john"), "NEWACCOUNT overdraft"));
     }
 
     @Test
     @DisplayName("Fails if NEWACCOUNT is lowercase -> For john: (newaccount moneymarket)")
-    public void TEST8() throws FileNotFoundException{
+    public void TESTh() throws FileNotFoundException{
         assertEquals("UNRECOGNIZED COMMAND.", bank.processRequest(new CustomerID("john"), "newaccount moneymarket"));
     }
 }
--- Testing/SHOWMYACCOUNTS_UnitTest.java
+++ Testing/SHOWMYACCOUNTS_UnitTest.java
@@ -16,31 +16,31 @@ public class SHOWMYACCOUNTS_UnitTest{
 
     @Test
     @DisplayName("Fails if SHOWMYACCOUNTS is lowercase")
-    public void TEST1() throws FileNotFoundException{
+    public void TESTa() throws FileNotFoundException{
         assertEquals("UNRECOGNIZED COMMAND.", bank.processRequest(new CustomerID("bhagy"), "showmyaccounts"));
     }
 
     @Test
     @DisplayName("Correct accounts return for Bhagy")
-    public void TEST2() throws FileNotFoundException{
-        assertEquals("CHECKING, 512469: 2220.0\nSAVINGS, 10673045: 1500.0", bank.processRequest(new CustomerID("bhagy"), "SHOWMYACCOUNTS"));
+    public void TESTb() throws FileNotFoundException{
+        assertEquals("CHECKING, 1512469: 2220.0\nSAVINGS, 20673045: 1500.0", bank.processRequest(new CustomerID("bhagy"), "SHOWMYACCOUNTS"));
     }
 
     @Test
     @DisplayName("Correct accounts return for John")
-    public void TEST3() throws FileNotFoundException{
-        assertEquals("CHECKING, 5058458: 25595.0\nSAVINGS, 14685857: 50.0", bank.processRequest(new CustomerID("john"), "SHOWMYACCOUNTS"));
+    public void TESTc() throws FileNotFoundException{
+        assertEquals("CHECKING, 15058458: 25595.0\nSAVINGS, 24685857: 50.0", bank.processRequest(new CustomerID("john"), "SHOWMYACCOUNTS"));
     }
 
     @Test
     @DisplayName("Correct accounts return for Christina")
-    public void TEST4() throws FileNotFoundException{
-        assertEquals("CHECKING, 7110635: 38460.0\nSAVINGS, 11864414: 965.0\nMONEYMARKET, 37966632: 2960.0", bank.processRequest(new CustomerID("christina"), "SHOWMYACCOUNTS"));
+    public void TESTd() throws FileNotFoundException{
+        assertEquals("CHECKING, 17110635: 38460.0\nSAVINGS, 21864414: 965.0", bank.processRequest(new CustomerID("christina"), "SHOWMYACCOUNTS"));
     }
 
     @Test
     @DisplayName("Correct accounts return for AmrElshenawy")
-    public void TEST5() throws FileNotFoundException{
-        assertEquals("CHECKING, 5890347: 100600.0", bank.processRequest(new CustomerID("amrelshenawy"), "SHOWMYACCOUNTS"));
+    public void TESTe() throws FileNotFoundException{
+        assertEquals("CHECKING, 15890347: 100600.0", bank.processRequest(new CustomerID("amrelshenawy"), "SHOWMYACCOUNTS"));
     }
 }
