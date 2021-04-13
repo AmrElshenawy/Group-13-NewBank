354d89472d8e67ae1c391ba3bc3e5616f3653573
Amhar <af965@bath.ac.uk>
2021-04-14 01:04:58 +0200

    modified timed logout function

--- newbank/client/ExampleClient.java
+++ newbank/client/ExampleClient.java
@@ -1,11 +1,17 @@
 package newbank.client;
 
+import newbank.server.DatabaseHandler;
+import newbank.server.NewBank;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.sql.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ExampleClient extends Thread{
 	
@@ -27,6 +33,9 @@ public class ExampleClient extends Thread{
 						String response = bankServerIn.readLine();
 						if (response != null){
 							System.out.println(response);
+							if(response.equals("SUCCESS. You have been logged out.")){
+								System.exit(0);
+							}
 						}
 					}
 				} catch (IOException e) {
@@ -42,14 +51,49 @@ public class ExampleClient extends Thread{
 	public void run() {
 		while(true) {
 			try {
+				TimerTask task = newTask();
+				timedExit("start", task);
 				while(true) {
 					String command = userInput.readLine();
+					timedExit("stop", task);
 					bankServerOut.println(command);
+					task = newTask();
+					timedExit("start", task);
+
 				}				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void timedExit(String instruction, TimerTask task){
+		Timer timer = new Timer();
+		if(instruction.equals("start")){
+			//System.out.println("started!");
+			timer.schedule(task, new Date(System.currentTimeMillis()+30*1000));
+		} else if(instruction.equals("stop")){
+			//System.out.println("stopped!");
+			task.cancel();
+		}
+	}
+
+	private TimerTask newTask(){
+		return new TimerTask() {
+			@Override
+			public void run() {
+				//save changes to database before closing
+//				DatabaseHandler save = new DatabaseHandler();
+//				NewBank bank = new NewBank();
+//				try {
+//					save.saveSession(bank.getName2CustomersMapping());
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+				System.out.println("You have been logged out due to inactivity");
+				System.exit(0);
+			}
+		};
 	}
 	
 	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
